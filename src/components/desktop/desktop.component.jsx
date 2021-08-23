94a57a71ea28fa65df953ee0a875e387cead00b1
Amasuo <oussamazay.1@gmail.com>
2021-08-23 01:21:23 +0100

    f

--- src/components/desktop/desktop.component.jsx
+++ src/components/desktop/desktop.component.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import './desktop.styles.css' 
+import CKEditor from 'ckeditor4-react';
+import ReactHtmlParser from 'react-html-parser';
+
 // import CKEditor from 'react-ckeditor-component';
 // import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function Desktop({data,setData}) {
-    const handleChange = ( event) => {
-        setData(data => data = event.target.value);
+    const handleChange = (event) => {
+        setData(data => data = event.editor.getData().replace(/<[^>]+>/g, ''));
         console.log('data :', data)
     }
     return (
         <div className="center">
             <h1>Text</h1>
-            <input
-                value={data}
-                onChange={handleChange}
+            <CKEditor
+                data= {data}
+                onChange = {handleChange}
             />
         </div>
     )
